995315da6a85185e7bc43da53371503353ec639d
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-12-12 15:14:14 +0100

    styling for android and ios

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -12,3 +12,23 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    //aos
+    elevation: 4,
+    //ios
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
