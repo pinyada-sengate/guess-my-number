46e5c182be10c6efbf38a87a136feb965b173f87
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-12-22 16:57:55 +0100

    game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,22 @@
-import { Text } from "react-native";
+import { view, Text, StyleSheet, View } from "react-native";
 
 function GameScreen() {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      {/* <View>LOG ROUND</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
